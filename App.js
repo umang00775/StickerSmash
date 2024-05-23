b3c4cd95f95250f3df0e2c480950b815dfb5b791
Umang Rathod <umang00775@gmail.com>
2024-05-23 11:05:29 +0530

    ImageViewer and Button created

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceHolderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceHolderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 50
+  },
+  
 });
