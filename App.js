537f8af47696dad6901acca1d1a43aa0160ce529
Umang Rathod <umang00775@gmail.com>
2024-05-23 12:03:40 +0530

    Image picker created

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 
 const PlaceHolderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if(result){
+      console.log(result);
+    }
+    else{
+      alert("Select some image nigga!")
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceHolderImage} />
       </View>
       <View style={styles.footerContainer} >
-        <Button label="Choose a photo" theme="primary" style={styles.button} />
+        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync} style={styles.button} />
         <Button label="Use this photo" style={styles.button} />
       </View>
       <StatusBar style="auto" />
